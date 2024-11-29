49fc2d6ce35c60795621a68dd76aa7c0af798bd4
paycardsx <paycards.negocios@gmail.com>
2024-11-29 03:14:29 -0300

    fix: corrige configuração do Firebase Storage

--- OneDrive/Área de Trabalho/SOFTWARE/payoca-tapiocas7/src/lib/firebase.ts
+++ OneDrive/Área de Trabalho/SOFTWARE/payoca-tapiocas7/src/lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBg2eMmlDSt3yJ2UIufbzS-JAr1RTFWzlI",
   authDomain: "payoca-tapiocas.firebaseapp.com",
   projectId: "payoca-tapiocas",
-  storageBucket: "payoca-tapiocas.firebasestorage.app",
+  storageBucket: "payoca-tapiocas.appspot.com",
   messagingSenderId: "222109159058",
   appId: "1:222109159058:web:46a771cdea7a1a0e46604b",
   measurementId: "G-BBQWQ12ZGN"
